05832c4ecaa0d7b2ec5f6e42b6288af1abc1f29f
chieunh2008 <chieunh.edu@gmail.com>
2025-06-23 14:03:34 +0700

    Firstchange

---
+++ TemplateRewriterTool/MainForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TemplateRewriterTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSelectTemplate = new Button();
+            lstKeys = new ListBox();
+            txtReplaceValue = new TextBox();
+            rtxPreview = new RichTextBox();
+            btnExport = new Button();
+            SuspendLayout();
+            // 
+            // btnSelectTemplate
+            // 
+            btnSelectTemplate.Location = new Point(23, 21);
+            btnSelectTemplate.Name = "btnSelectTemplate";
+            btnSelectTemplate.Size = new Size(121, 23);
+            btnSelectTemplate.TabIndex = 0;
+            btnSelectTemplate.Text = "Chọn File template";
+            btnSelectTemplate.UseVisualStyleBackColor = true;
+            btnSelectTemplate.Click += btnSelectTemplate_Click;
+            // 
+            // lstKeys
+            // 
+            lstKeys.FormattingEnabled = true;
+            lstKeys.ItemHeight = 15;
+            lstKeys.Location = new Point(23, 66);
+            lstKeys.Name = "lstKeys";
+            lstKeys.Size = new Size(121, 319);
+            lstKeys.TabIndex = 1;
+            lstKeys.SelectedIndexChanged += lstKeys_SelectedIndexChanged;
+            // 
+            // txtReplaceValue
+            // 
+            txtReplaceValue.Location = new Point(23, 401);
+            txtReplaceValue.Name = "txtReplaceValue";
+            txtReplaceValue.Size = new Size(363, 23);
+            txtReplaceValue.TabIndex = 2;
+            txtReplaceValue.TextChanged += txtReplaceValue_TextChanged;
+            // 
+            // rtxPreview
+            // 
+            rtxPreview.Location = new Point(164, 21);
+            rtxPreview.Name = "rtxPreview";
+            rtxPreview.ReadOnly = true;
+            rtxPreview.Size = new Size(615, 364);
+            rtxPreview.TabIndex = 3;
+            rtxPreview.Text = "";
+            // 
+            // btnExport
+            // 
+            btnExport.Location = new Point(630, 401);
+            btnExport.Name = "btnExport";
+            btnExport.Size = new Size(149, 23);
+            btnExport.TabIndex = 4;
+            btnExport.Text = "Xuất File";
+            btnExport.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnExport);
+            Controls.Add(rtxPreview);
+            Controls.Add(txtReplaceValue);
+            Controls.Add(lstKeys);
+            Controls.Add(btnSelectTemplate);
+            Name = "MainForm";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSelectTemplate;
+        private ListBox lstKeys;
+        private TextBox txtReplaceValue;
+        private RichTextBox rtxPreview;
+        private Button btnExport;
+    }
+}
---
+++ TemplateRewriterTool/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TemplateRewriterTool
+{
+    public partial class MainForm : Form
+    {
+        private Dictionary<string, string> keyReplacements = new Dictionary<string, string>();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectTemplate_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string content = File.ReadAllText(ofd.FileName);
+                rtxPreview.Text = content;
+
+                // Tìm tất cả các key dạng {{Ten}}
+                var matches = Regex.Matches(content, @"{{(.*?)}}");
+
+                lstKeys.Items.Clear();
+                foreach (Match match in matches)
+                {
+                    string key = match.Groups[1].Value;
+                    if (!lstKeys.Items.Contains(key))
+                        lstKeys.Items.Add(key);
+                }
+            }
+        }
+
+        private void lstKeys_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstKeys.SelectedItem != null)
+            {
+                string selectedKey = lstKeys.SelectedItem.ToString();
+
+                if (keyReplacements.ContainsKey(selectedKey))
+                    txtReplaceValue.Text = keyReplacements[selectedKey];
+                else
+                    txtReplaceValue.Clear();
+            }
+        }
+
+        private void txtReplaceValue_TextChanged(object sender, EventArgs e)
+        {
+            if (lstKeys.SelectedItem != null)
+            {
+                string key = lstKeys.SelectedItem.ToString();
+                keyReplacements[key] = txtReplaceValue.Text;
+            }
+
+        }
+    }
+}
